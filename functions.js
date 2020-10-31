9c0b8743bd4129578a53c4daddb6d8465e642130
cl0ne <cl0ne@mithril.org.ua>
2020-10-31 04:51:23 +0200

    Replace jQuery with vanilla JS

--- functions.js
+++ functions.js
@@ -1,19 +1,28 @@
-$( document ).ready (function () {
-    $("pre.src-sh").each(function () {
-	var lines = $( this ).text().split('\n');
-	for (var ind = 0 ; ind < lines.length ; ind++) {
-            if ( ! lines[ind] ) {
-		continue;
-	    }
-	    lines[ind] = '<span class="shell-line"></span>' + lines[ind];
-	}
-	$( this ).html(lines.join("\n"));
+function ready(callback) {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+}
+
+ready(function () {
+    document.querySelectorAll("pre.src-sh").forEach(function (e) {
+        var lines = e.textContent.split("\n");
+        for (var i = 0; i < lines.length; ++i) {
+            var text = lines[i];
+            if (!text || (text[0] == " ") | (text[0] == "\t")) {
+                continue;
+            }
+            lines[i] = '<span class="shell-line"></span>' + text;
+        }
+        e.innerHTML = lines.join("\n");
     });
 
-
     // broken way to do things for now.
-    $( "div.status" ).prepend( $('<br><a href="index.org">org source</a>') )
-    $( "div.status" ).prepend( $('<hr></hr>') )
-
+    var footer = document.querySelector("div.status");
+    var sourceBlock = document.createElement("p");
+    var sourceLink = document.createElement("a");
+    sourceLink.href = "index.org";
+    sourceLink.textContent = "org source";
+    sourceBlock.appendChild(sourceLink);
+    footer.insertBefore(sourceBlock, footer.children[0]);
+    footer.insertBefore(document.createElement("hr"), sourceBlock);
 });
-
